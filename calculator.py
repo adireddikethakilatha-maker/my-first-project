e2ecd8a6df6f31894e764142784286c5dbfd3f7d
adireddikethakilatha-maker <adireddikethakilatha@gmail.com>
2026-09-12 21:19:01 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
-a = float(input("Enter first number: 10"))
-b = float(input("Enter second number: 20"))
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
 
 sum = a + b
 
-print("The answer is:30", sum)
+print("The answer is:", sum)
