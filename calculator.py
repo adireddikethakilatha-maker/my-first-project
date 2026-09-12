2db64cc0e70f41f82c2fa1e765ef428fad96ab0c
adireddikethakilatha-maker <adireddikethakilatha@gmail.com>
2026-09-12 21:21:26 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,7 +1,20 @@
 a = float(input("Enter first number: "))
 b = float(input("Enter second number: "))
 
-print("Addition:", a + b)
-print("Subtraction:", a - b)
-print("Multiplication:", a * b)
-print("Division:", a / b)
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+
+choice = input("Choose an operation: ")
+
+if choice == "1":
+    print("Answer:", a + b)
+elif choice == "2":
+    print("Answer:", a - b)
+elif choice == "3":
+    print("Answer:", a * b)
+elif choice == "4":
+    print("Answer:", a / b)
+else:
+    print("Invalid choice")
